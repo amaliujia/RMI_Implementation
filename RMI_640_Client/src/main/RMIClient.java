b886b3db566f5d4954e8e71b7db5b545e2560587
pystone <pystone9205@gmail.com>
2014-10-10 22:33:38 -0400

    update report

--- RMI_640_Client/src/main/RMIClient.java
+++ RMI_640_Client/src/main/RMIClient.java
@@ -35,10 +35,19 @@ public class RMIClient {
 				ror = registry.lookup("StudentList", null, null);
 			}
 			StudentList_stub stuList = (StudentList_stub)ror.localize();
+			
+			/* remotely invoke method to generate new student in StudentList */
 			stuList.generateNewStudent("Yan Pan", 100);
 			stuList.generateNewStudent("Kailiang Chen", 100);
 			stuList.generateNewStudent("Kasden Greg", 59);
 			
+			/* Get the first student in the list, an example of pass by reference (because Student is subclass 
+			 * of RMIService.
+			 * 
+			 * Then we remotely invoke two methods to modify it and two to get the value of it. The "getName"
+			 * and "getScore" methods are the examples of pass by value (because String and int are not subclass
+			 * of RMIService.
+			 */
 			Student_stub yp = stuList.getFirstStudent();
 			System.out.println("Before name modify: " + yp.getName() + " has score " + yp.getScore());
 			yp.setName("Yang Pan");
@@ -46,6 +55,9 @@ public class RMIClient {
 			yp.setScore(80); /* :( */
 			System.out.println("After score modify: " + yp.getName() + " has score " + yp.getScore());
 			
+			/* to illustrate the student is really passed by reference, we get a new remote object reference
+			 * here to see its value
+			 */
 			Student_stub new_yp = stuList.getFirstStudent();
 			System.out.println("Finally: " + new_yp.getName() + " has score " + new_yp.getScore());
 		} catch (RMIException e1) {
